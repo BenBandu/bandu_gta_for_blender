e2c65d218c1cb8310ecb9a89da4f8f76255106d2
BenBandu <simonrofstad@outlook.com>
2023-08-20 14:19:05 +0200

    Add ped selection to merge replay panel

--- replay_manager/operators/replay/merge_replays.py
+++ replay_manager/operators/replay/merge_replays.py
@@ -6,7 +6,26 @@ class RM_OT_MergeReplays(bpy.types.Operator):
 	bl_idname = "replay_manager.merge_replay"
 	bl_label = "Merge Replays"
 
-	def get_mergee_items(self, context):
+	def update_merger_data(self, context):
+		manager = context.scene.replay_manager
+
+		vehicles = []
+		for vehicle in manager.active_replay.vehicles:
+			if vehicle.target:
+				name = str(vehicle.index)
+				vehicles.append((name, name, vehicle.model, "", 1 << len(vehicles)))
+
+		RM_OT_MergeReplays._merger_vehicle_items = tuple(vehicles)
+
+		peds = []
+		for ped in manager.active_replay.peds:
+			if ped.target:
+				name = str(ped.index)
+				peds.append((name, name, ped.model, "", 1 << len(peds)))
+
+		RM_OT_MergeReplays._merger_ped_items = tuple(peds)
+
+	def get_mergee_options(self, context):
 		manager = context.scene.replay_manager
 		replays = [("-1", "Select Replay...", "")]
 		for i, replay in enumerate(manager.replays):
@@ -17,18 +36,7 @@ class RM_OT_MergeReplays(bpy.types.Operator):
 
 		return tuple(replays)
 
-	def set_merger_vehicles(self, context):
-		manager = context.scene.replay_manager
-
-		vehicles = []
-		for vehicle in manager.active_replay.vehicles:
-			if vehicle.target:
-				name = str(vehicle.index)
-				vehicles.append((name, name, vehicle.model, "", 1 << len(vehicles)))
-
-		RM_OT_MergeReplays._merger_vehicles_items = tuple(vehicles)
-
-	def set_mergee_vehicles(self, context):
+	def update_mergee_data(self, context):
 		if self.mergee == "-1":
 			return ()
 
@@ -40,40 +48,64 @@ class RM_OT_MergeReplays(bpy.types.Operator):
 				name = str(vehicle.index)
 				vehicles.append((name, name, vehicle.model, "", 1 << len(vehicles)))
 
-		RM_OT_MergeReplays._mergee_vehicles_items = tuple(vehicles)
+		RM_OT_MergeReplays._mergee_vehicle_items = tuple(vehicles)
 
-	def get_merger_vehicles_items(self, context):
-		return RM_OT_MergeReplays._merger_vehicles_items
+		peds = []
+		for ped in manager.replays[int(self.mergee)].peds:
+			if ped.target:
+				name = str(ped.index)
+				peds.append((name, name, ped.model, "", 1 << len(peds)))
 
-	def get_mergee_vehicles_items(self, context):
-		return RM_OT_MergeReplays._mergee_vehicles_items
+		RM_OT_MergeReplays._mergee_ped_items = tuple(peds)
+
+	get_merger_vehicle_items = lambda self, context: RM_OT_MergeReplays._merger_vehicle_items
+	get_merger_ped_items     = lambda self, context: RM_OT_MergeReplays._merger_ped_items
+	get_mergee_vehicle_items = lambda self, context: RM_OT_MergeReplays._mergee_vehicle_items
+	get_mergee_ped_items     = lambda self, context: RM_OT_MergeReplays._mergee_ped_items
+
+	_merger_vehicle_items = ()
+	_merger_ped_items     = ()
+	_mergee_vehicle_items = ()
+	_mergee_ped_items     = ()
 
 	mergee: bpy.props.EnumProperty(
-		items=get_mergee_items,
+		items=get_mergee_options,
 		name="Replays",
 		description="Replay to merge with",
-		update=set_mergee_vehicles,
+		update=update_mergee_data,
 	)
 
-	_merger_vehicles_items = ()
 	merger_vehicles: bpy.props.EnumProperty(
-		items=get_merger_vehicles_items,
+		items=get_merger_vehicle_items,
 		name="Vehicles",
 		description="Vehicles from Merger",
 		options={"ENUM_FLAG"}
 	)
 
-	_mergee_vehicles_items = ()
+	merger_peds: bpy.props.EnumProperty(
+		items=get_merger_ped_items,
+		name="Peds",
+		description="Peds from Merger",
+		options={"ENUM_FLAG"}
+	)
+
 	mergee_vehicles: bpy.props.EnumProperty(
-		items=get_mergee_vehicles_items,
+		items=get_mergee_vehicle_items,
 		name="Vehicles",
 		description="Vehicles from Mergee",
 		options={"ENUM_FLAG"}
 	)
 
+	mergee_peds: bpy.props.EnumProperty(
+		items=get_mergee_ped_items,
+		name="Peds",
+		description="Peds from Mergee",
+		options={"ENUM_FLAG"}
+	)
+
 	def invoke(self, context, event):
-		self.set_merger_vehicles(context)
-		self.set_mergee_vehicles(context)
+		self.update_merger_data(context)
+		self.update_mergee_data(context)
 
 		wm = context.window_manager
 		return wm.invoke_props_dialog(self)
@@ -84,7 +116,7 @@ class RM_OT_MergeReplays(bpy.types.Operator):
 
 		if self.mergee == "-1":
 			self.reset()
-			return
+			return {"FINISHED"}
 
 		self.reset()
 		return {"FINISHED"}
@@ -94,8 +126,10 @@ class RM_OT_MergeReplays(bpy.types.Operator):
 
 	def reset(self):
 		self.mergee = "-1"
-		self.__class__._merger_vehicles_items = ()
-		self.__class__._mergee_vehicles_items = ()
+		self.__class__._merger_vehicle_items = ()
+		self.__class__._mergee_vehicle_items = ()
+		self.__class__._merger_ped_items = ()
+		self.__class__._mergee_ped_items = ()
 
 	def draw(self, context):
 		manager = context.scene.replay_manager
@@ -113,23 +147,45 @@ class RM_OT_MergeReplays(bpy.types.Operator):
 		row.prop(self, "mergee", text="Mergee", )
 
 		self.layout.separator()
+		row = self.layout.row()
+		row.label(text="Merger:")
 
 		row = self.layout.row()
-		row.label(text="Merger Vehicles:")
+		row.scale_y = 0.5
+		row.label(text="Vehicles")
+		row.label(text="Peds")
 
-		grid = self.layout.grid_flow(columns=5, even_columns=True, align=True)
-		for flag in self._merger_vehicles_items:
+		row = self.layout.row()
+		split = row.split(factor=0.5)
+		grid = split.column().grid_flow(columns=5, even_columns=True, align=True)
+		for flag in self._merger_vehicle_items:
 			grid.prop_enum(self, "merger_vehicles", flag[0])
 
-		if self.mergee != "-1":
-			row = self.layout.row()
-			row.label(text="Mergee Vehicles:")
+		grid = split.column().grid_flow(columns=5, even_columns=True, align=True)
+		for flag in self._merger_ped_items:
+			grid.prop_enum(self, "merger_peds", flag[0])
 
-			grid = self.layout.grid_flow(columns=5, even_columns=True, align=True)
-			for flag in self._mergee_vehicles_items:
+		if self.mergee != "-1":
+			self.layout.separator()
+
+			row = self.layout.row()
+			row.label(text="Mergee:")
+
+			row = self.layout.row()
+			row.scale_y = 0.5
+			row.label(text="Vehicles")
+			row.label(text="Peds")
+
+			row = self.layout.row()
+			split = row.split(factor=0.5)
+			grid = split.column().grid_flow(columns=5, even_columns=True, align=True)
+			for flag in self._mergee_vehicle_items:
 				grid.prop_enum(self, "mergee_vehicles", flag[0])
 
-		#TODO: Ped flags
+			grid = split.column().grid_flow(columns=5, even_columns=True, align=True, )
+			for flag in self._mergee_ped_items:
+				grid.prop_enum(self, "mergee_peds", flag[0])
+
 		#TODO: Handle potential re-assign model ids?
 		#TODO: Settings...
 
